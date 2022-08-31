a54d3fe8e92e65eda23b40b3bbb5322caed8df2b
vitor <vitorsbsilva@gmail.com>
2022-08-31 12:22:46 -0300

    feat: route Get Sales By Id

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -3,9 +3,25 @@ const salesServices = require('../services/salesServices');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/', async (_req, res) => {
     try {
         const allSales = await salesServices.getAll();
+        return res.status(200).json(allSales);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Algo deu errado' });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const allSales = await salesServices.getById(id);
+
+        if (allSales.message) {
+            return res.status(404).json(allSales);
+        }
+        
         return res.status(200).json(allSales);
     } catch (error) {
         console.log(error);
--- services/salesServices.js
+++ services/salesServices.js
@@ -8,6 +8,19 @@ const getAll = async () => {
     return allSales;
 };
 
+const getById = async (id) => {
+    const saleById = await Sales.findOne({
+        attributes: { exclude: ['createdAt', 'updatedAt'] },
+        // eslint-disable-next-line object-shorthand
+        where: { id: id },
+    });
+
+    if (saleById === null) return { message: 'Sale Not Found' };
+
+    return saleById;
+};
+
 module.exports = {
     getAll,
+    getById,
 };
