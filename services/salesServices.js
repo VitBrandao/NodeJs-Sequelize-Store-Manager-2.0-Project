29d3b97f26f1aa2a0cb44290debd556bda0a1493
vitor <vitorsbsilva@gmail.com>
2022-08-31 12:44:24 -0300

    fix: salesServices

--- services/salesServices.js
+++ services/salesServices.js
@@ -1,18 +1,26 @@
-const { Sales } = require('../models');
+const { SalesProducts } = require('../models');
 
 const getAll = async () => {
-    const allSales = await Sales.findAll({
-        attributes: { exclude: ['createdAt', 'updatedAt'] },
+    const allSales = await SalesProducts.findAll({        
+        attributes: { exclude: ['id', 'createdAt', 'updatedAt'] },
+        // include: [
+        //     {
+        //       model: Sales,
+        //       as: 'sale',
+        //       attributes: ['date'],
+        //     },
+        // ],
     });
 
     return allSales;
 };
 
 const getById = async (id) => {
-    const saleById = await Sales.findOne({
-        attributes: { exclude: ['createdAt', 'updatedAt'] },
+    const saleById = await SalesProducts.findOne({
+        attributes: { exclude: ['id', 'createdAt', 'updatedAt'] },
         // eslint-disable-next-line object-shorthand
-        where: { id: id },
+        where: { saleId: id },
+
     });
 
     if (saleById === null) return { message: 'Sale Not Found' };
