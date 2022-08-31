527582f87dbc2435cb2081cdf180dfa25aa72eda
vitor <vitorsbsilva@gmail.com>
2022-08-31 11:47:55 -0300

    feat: index.js add productsController to app.use

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+const express = require('express');
+const productController = require('./controllers/productsController');
+
+const app = express();
+app.use(express.json());
+
 require('dotenv').config();
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', productController);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
