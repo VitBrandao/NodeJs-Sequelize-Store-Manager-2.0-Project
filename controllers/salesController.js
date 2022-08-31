481b9b9c62df372f7d9b25773b77b67ef346ec9f
vitor <vitorsbsilva@gmail.com>
2022-08-31 18:59:18 -0300

    feat: route Post to Create a new Sale with middleware validations

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -17,12 +17,27 @@ router.get('/:id', async (req, res) => {
     try {
         const { id } = req.params;
         const allSales = await salesServices.getById(id);
-
+        
         if (allSales.message) {
             return res.status(404).json(allSales);
         }
         
         return res.status(200).json(allSales);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Algo deu errado' });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const createSale = await salesServices.create(req.body);
+
+        if (createSale.message) {
+            return res.status(createSale.status).json(createSale.message);
+        }
+
+        return res.status(201).json(createSale);
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: 'Algo deu errado' });
--- services/salesServices.js
+++ services/salesServices.js
@@ -1,34 +1,60 @@
-const { SalesProducts } = require('../models');
+const { 
+    validateProductId, 
+    validateQuantity, 
+    mountCreatedObject } = require('../middlewares/validateSales');
+const { SalesProducts, Sales } = require('../models');
 
 const getAll = async () => {
     const allSales = await SalesProducts.findAll({        
-        attributes: { exclude: ['id', 'createdAt', 'updatedAt'] },
-        // include: [
-        //     {
-        //       model: Sales,
-        //       as: 'sale',
-        //       attributes: ['date'],
-        //     },
-        // ],
+        attributes: { exclude: ['id'] },
     });
 
     return allSales;
 };
 
 const getById = async (id) => {
-    const saleById = await SalesProducts.findOne({
-        attributes: { exclude: ['id', 'createdAt', 'updatedAt'] },
-        // eslint-disable-next-line object-shorthand
-        where: { saleId: id },
-
+    const paramsId = id;
+    const saleById = await SalesProducts.findAll({
+        attributes: { exclude: ['id'] },
+        where: { saleId: paramsId },
     });
 
-    if (saleById === null) return { message: 'Sale Not Found' };
+    const saleNull = saleById === null;
+    const saleUndef = saleById === undefined;
+    const saleIsEmpty = saleById.length === 0;
+
+    if (saleNull || saleUndef || saleIsEmpty) return { message: 'Sale Not Found' };
 
     return saleById;
+};
+
+const create = async (body) => {
+    const { quantity: qty, productId: prodId } = body;
+
+    const prodIdVerifications = validateProductId(prodId);
+    if (prodIdVerifications.message) return prodIdVerifications;
+
+    const qtyVerifications = validateQuantity(qty);
+    if (qtyVerifications.message) return qtyVerifications;
+
+    const createSale = await Sales.create({
+        date: Date.now(),
+    });
+
+    const newSaleId = createSale.dataValues.id;
+
+    const createSaleProduct = await SalesProducts.create({
+        saleId: newSaleId,
+        productId: prodId, 
+        quantity: qty, 
+    }, { fields: ['saleId', 'productId', 'quantity'] });
+
+    const createdObject = mountCreatedObject(newSaleId, createSaleProduct.dataValues);
+    return createdObject;
 };
 
 module.exports = {
     getAll,
     getById,
+    create,
 };
